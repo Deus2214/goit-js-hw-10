bf739a67eb3486d86881e8062250860428dc6622
Vitalii Gritsun <110825559+Deus2214@users.noreply.github.com>
2023-01-06 13:52:22 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,97 +1,79 @@
 import './css/styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from './js/fetchCountries';
+import { countryListMarkup } from './js/countryListMarkup';
+import { countryInfoMarkup } from './js/countryInfoMarkup';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
-const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
-const infoMessage =
-  'Too many matches found. Please enter a more specific name.';
-const errorMessage = 'Oops, there is no country with that name';
-const notifyOptions = {
-  showOnlyTheLastOne: true,
-  clickToClose: true,
-  cssAnimationStyle: 'zoom',
-};
 
-const refs = {
-  input: document.querySelector('#search-box'),
-  outputList: document.querySelector('.country-list'),
-  outputCountryInfo: document.querySelector('.country-info'),
-};
+const inputValue = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
-refs.input.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
+// Назву країни для пошуку користувач вводить у текстове поле input#search-box.
+// HTTP-запити виконуються при введенні назви країни, тобто на події input.
+// Але робити запит з кожним натисканням клавіші не можна, оскільки одночасно буде багато запитів
+// і вони будуть виконуватися в непередбачуваному порядку.
+// Необхідно застосувати прийом Debounce на обробнику події і робити HTTP-запит через 300 мс після того,
+// як користувач перестав вводити текст. Використовуй пакет lodash.debounce.
+inputValue.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 
-// set function
-function onFormInput() {
-  const countryName = refs.input.value.trim();
-  if (!countryName) {
-    clearResultOutput();
+function onInputSearch(event) {
+  // Якщо користувач повністю очищає поле пошуку, то HTTP-запит не виконується,
+  // а розмітка списку країн або інформації про країну зникає.
+  if (!event.target.value) {
+    emptyInput();
+    Notiflix.Notify.warning('Please enter any data!');
     return;
-  } else {
-    fetchCountries(countryName).then(showCountries).catch(handleError);
   }
+
+  // Виконай санітизацію введеного рядка методом trim(), це вирішить проблему,
+  // коли в полі введення тільки пробіли, або вони є на початку і в кінці рядка.
+  return (
+    fetchCountries(event.target.value.trim())
+      .then(showCountry)
+      // Якщо користувач ввів назву країни, якої не існує, бекенд поверне не порожній масив, а помилку зі статус кодом 404 - не знайдено.
+      // Якщо це не обробити, то користувач ніколи не дізнається про те, що пошук не дав результатів.
+      .catch(showError)
+  );
 }
 
-function showCountries(countriesData) {
-  if (countriesData.length > 10) {
-    clearResultOutput();
-    Notify.info(infoMessage, notifyOptions);
+function showCountry(countries) {
+  // Якщо у відповіді бекенд повернув більше ніж 10 країн, в інтерфейсі з'являється повідомлення про те,
+  // що назва повинна бути специфічнішою.
+  // Для повідомлень використовуй бібліотеку notiflix і виводь такий рядок "Too many matches found. Please enter a more specific name.".
+  if (countries.length > 10) {
+    emptyInput();
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    return;
   }
-  if (countriesData.length > 1 && countriesData.length <= 10) {
-    clearResultOutput();
-    showCountriesList(countriesData);
+
+  // Якщо бекенд повернув від 2-х до 10-и країн, під тестовим полем відображається список знайдених країн.
+  // Кожен елемент списку складається з прапора та назви країни.
+  if (countries.length >= 2 && countries.length <= 10) {
+    emptyInput();
+    countryList.innerHTML = countryListMarkup(countries);
+    return;
   }
-  if (countriesData.length === 1) {
-    clearResultOutput();
-    showCountryInfo(countriesData);
-  }
+
+  // Якщо результат запиту - це масив з однією країною, в інтерфейсі відображається розмітка картки
+  // з даними про країну: прапор, назва, столиця, населення і мови.
+  emptyInput();
+  countryInfo.innerHTML = countryInfoMarkup(countries);
 }
 
-// render one country, which satisfy the request
-function showCountryInfo(countriesData) {
-  const receivedItem = getData(countriesData);
-  const { capital, name, population, flagSrc, languages } = receivedItem[0];
-  refs.outputCountryInfo.innerHTML = `<div class="country-name__wrapper">
-      <img class="country-flag" src="${flagSrc}" alt="flag of the country ${name}">
-      <h1 class="country-name">${name}</h1>
-    </div><p class="country-info__text"><b>Capital: </b>${capital}</p>
-    <p class="country-info__text"><b>Population: </b>${population}</p>
-    <p class="country-info__text"><b>Languages: </b>${languages}</p>`;
+// Додай повідомлення "Oops, there is no country with that name" у разі помилки, використовуючи бібліотеку notiflix.
+function showError() {
+  emptyInput();
+  Notiflix.Notify.failure('Oops, there is no country with that name');
 }
 
-// render list of the countries, which satisfy the request
-function showCountriesList(countriesData) {
-  const countriesList = getData(countriesData);
-  refs.outputList.innerHTML = countriesList
-    .map(
-      ({
-        flagSrc,
-        name,
-      }) => `<li class="country-list__item"><img class="country-flag--sm" src="${flagSrc}" alt="flag of the country ${name}" />
-      <p class="country-name--sm">${name}</p></li>`
-    )
-    .join('');
+function emptyInput() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
 }
 
-// receive necessary data from backend's fetch
-function getData(countriesData) {
-  return countriesData.map(elem => {
-    return {
-      capital: elem.capital.join(', '),
-      name: elem.name.official,
-      population: elem.population,
-      flagSrc: elem.flags.svg,
-      languages: Object.values(elem.languages).join(', '),
-    };
-  });
-}
 
-function clearResultOutput() {
-  refs.outputCountryInfo.innerHTML = '';
-  refs.outputList.innerHTML = '';
-}
-
-function handleError(error) {
-  clearResultOutput();
-  Notify.failure(errorMessage, notifyOptions);
-}
